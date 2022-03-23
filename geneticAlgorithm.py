f66321174dddf17710cb0b42aab068d15ff5d501
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-03-23 22:55:46 +0700

    cleared some stuff

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -6,37 +6,34 @@ def h(x,y) :
     hasil = (pow((cos(x) + sin(y)),2)) / pow(x,2)+pow(y,2)
     return hasil
 #Creating a population
-def generate_population(population, chromosome) :
-    return  [[random.choice([0,1]) for _ in range(chromosome_size)] for _ in range(population_size)]
+def generate_genome(genome_size):
+    pass
+
+def generate_population(population, genome_size) :
+    return  [[random.choice([0,1]) for _ in range(genome_size)] for _ in range(population_size)]
 
 #Decode Cromosome
-def decode(chromosome) :
-    x_max = 5
-    x_min = -5
-
-    y_max = 5
-    y_min = -5
-
+def decode(genome) : # Cepet2an
+    pass
     #domain function
     # d_function = lambda rmax,rmin, start_bit, end_bit :
 
 #Fitness Function
-def fitness(chromosome) :
-    x,y = decode(chromosome)
-    return(x,y)
+def fitness(genome) : # Talitha
+    pass
 
 #Parent Picking
-def tournament_selection(population, k) :
-    candidate = [population[random.randrange(0,len(population))] for _ in range(k)]
+def elitism(population, k) :
+    pass
 
 #Crossover
 def crossover(parent1, parent2, crossover_rate) :
-    
-#Mutation
-def mutation(chromosome, mutation rate) :
-    
-#New Generation
+    pass
 
+#Mutation
+def mutation(genome, mutation rate) : # Talitha
+    pass
+#New Generation
 
 def main():
     x = 10
